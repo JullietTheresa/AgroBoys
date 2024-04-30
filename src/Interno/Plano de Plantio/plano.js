83462c8ce5a93759dfaa27ace78fcbde65a6a6b4
Júlia Theresa Borges Cabral dos Santos <juliathcabral@.com>
2024-04-30 00:49:05 -0300

    Plano de Plantio parcialmente feito

--- src/Interno/Plano de Plantio/plano.js
+++ src/Interno/Plano de Plantio/plano.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styleplano.css";
 
-export const PlanoPlantio = () => {
+const PlanoPlantio = () => {
+  const [isColhido, setIsColhido] = useState(false);
+
+  const handleClick = () => {
+    setIsColhido(!isColhido);
+  };
+
   return (
     <div className="plano-plantio">
       <div className="overlap-wrapper">
         <div className="overlap">
           <div className="overlap-group">
+            {/* Elementos compartilhados */}
             <div className="rectangle" />
             <div className="ellipse" />
             <div className="text-wrapper">S</div>
@@ -15,39 +22,11 @@ export const PlanoPlantio = () => {
             </a>
             <div className="text-wrapper-2">admin</div>
             <div className="flexcontainer">
-              <p className="text">
-                <span className="span">
-                  1<br />
-                </span>
-              </p>
-              <p className="text">
-                <span className="span">
-                  2<br />
-                </span>
-              </p>
-              <p className="text">
-                <span className="span">
-                  3<br />
-                </span>
-              </p>
-              <p className="text">
-                <span className="span">
-                  4<br />
-                </span>
-              </p>
-              <p className="text">
-                <span className="span">
-                  5<br />
-                </span>
-              </p>
-              <p className="text">
-                <span className="span">
-                  6<br />
-                </span>
-              </p>
-              <p className="text">
-                <span className="span">7</span>
-              </p>
+              {[1, 2, 3, 4, 5, 6, 7].map((num) => (
+                <div key={num} className="text">
+                  <span className="span">{num}</span>
+                </div>
+              ))}
             </div>
             <div className="text-wrapper-3">Orders</div>
             <div className="rectangle-2" />
@@ -112,14 +91,18 @@ export const PlanoPlantio = () => {
               </div>
             </div>
           </div>
+          {/* Elementos específicos de Plano Plantio */}
           <div className="text-wrapper-21">Plano Plantio</div>
+          <p className="cultura">Cultura Selecionada: </p>
           <div className="overlap-3">
             <div className="group-4">
               <div className="overlap-4">
+                <div className="text-wrapper-35">Cenoura</div>
                 <div className="text-wrapper-22">24 h - 1h/dia</div>
                 <div className="text-wrapper-23">20/12/2024</div>
                 <div className="text-wrapper-24">Atrazina</div>
                 <div className="text-wrapper-25">10/12/2024</div>
+                <div className="text-wrapper-220">10/11/2024</div>
                 <img className="line" alt="Line" src="https://c.animaapp.com/AneYV77i/img/line-1.svg" />
                 <img className="line-2" alt="Line" src="https://c.animaapp.com/AneYV77i/img/line-3.svg" />
                 <img className="line-3" alt="Line" src="https://c.animaapp.com/AneYV77i/img/line-2.svg" />
@@ -127,28 +110,24 @@ export const PlanoPlantio = () => {
                 <img className="line-5" alt="Line" src="https://c.animaapp.com/AneYV77i/img/line-6.svg" />
                 <img className="line-6" alt="Line" src="https://c.animaapp.com/AneYV77i/img/line-7.svg" />
                 <img className="line-7" alt="Line" src="https://c.animaapp.com/AneYV77i/img/line-8.svg" />
-                <div className="group-5">
-                  <div className="overlap-group-2">
-                    <div className="rectangle-8" />
-                    <div className="text-wrapper-26">Não colhido</div>
-                  </div>
-                </div>
-                <div className="text-wrapper-27">10/09/2024</div>
               </div>
             </div>
-            <div className="text-wrapper-28">Cultura</div>
-            <div className="text-wrapper-29">Data Fertilização</div>
-            <div className="text-wrapper-30">Data Plantio</div>
-            <div className="text-wrapper-31">Agrotóxico</div>
-            <div className="text-wrapper-32">Data Agrotóxico</div>
-            <div className="text-wrapper-33">Colheita</div>
-            <div className="text-wrapper-34">Irrigação</div>
-            <div className="text-wrapper-35">Cenoura</div>
+            <div className="menu-plano">
+              <div className="text-wrapper-28">Ordem de Cultura</div>
+              <div className="text-wrapper-29">Data Fertilização</div>
+              <div className="text-wrapper-30">Data Plantio</div>
+              <div className="text-wrapper-31">Agrotóxico</div>
+              <div className="text-wrapper-32">Data Agrotóxico</div>
+              <div className="text-wrapper-33">Colheita</div>
+              <div className="text-wrapper-34">Irrigação</div>
+            </div>
             <div className="text-wrapper-36">Batata</div>
             <div className="text-wrapper-37">Milho</div>
             <div className="group-6">
               <div className="overlap-5">
-                <div className="text-wrapper-38">Colhido</div>
+                <button className="button" onClick={handleClick}>
+                  <div className="text-wrapper-38">{isColhido ? "Colhido" : "Não colhido"}</div>
+                </button>
               </div>
             </div>
           </div>
@@ -158,4 +137,4 @@ export const PlanoPlantio = () => {
   );
 };
 
-export default PlanoPlantio
+export default PlanoPlantio;
